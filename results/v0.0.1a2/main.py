cf51f92c283125e06f7020ee3fefc493e3454d88
Gil Ramot <gilramot@github.com>
2024-08-01 14:42:10 +0300

    Added to .gitignore

--- results/v0.0.1a2/main.py
+++ results/v0.0.1a2/main.py
@@ -3,16 +3,18 @@ import pandas as pd
 import modelcomp as mc
 
 import os
-
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
 import xgboost as xgb
 from sklearn.model_selection import RepeatedKFold
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
 
 
-def get_k_fold(seed=None, splits=5, repeats=5):
+def get_k_fold(seed=None, splits=5, repeats=2):
     return (
         RepeatedKFold(random_state=seed, n_splits=splits, n_repeats=repeats)
         if seed is not None
@@ -21,132 +23,70 @@ def get_k_fold(seed=None, splits=5, repeats=5):
 
 
 def get_models(seed=None, seed_dict=None):
-    return [
-        RandomForestClassifier(random_state=(seed if seed is not None else None)),
-        xgb.XGBClassifier(random_state=(seed if seed is not None else None)),
-        LogisticRegression(
-            random_state=(seed if seed is not None else None), max_iter=10000
-        ),
-        SVC(
+    """
+    xgb.XGBClassifier(random_state=(seed if seed is not None else None)),
+    LogisticRegression(
+        random_state=(seed if seed is not None else None), max_iter=10000
+    ),
+    SVC(
             random_state=(seed if seed is not None else None),
             kernel="linear",
             probability=True,
         ),
-        KNeighborsClassifier(),
+    KNeighborsClassifier(),
+    """
+    return [
+        RandomForestClassifier(random_state=(seed if seed is not None else None)),
+        DecisionTreeClassifier(random_state=(seed if seed is not None else None)),
     ]
 
 
 if __name__ == "__main__":
     print("Program started")
+    plt.rcParams["figure.constrained_layout.use"] = True
     validation_count = 1
     # adding dictionary of model names abbreviations
     imported_abundance = pd.read_csv(
-        os.getcwd() + "/../../data/abundance.csv", index_col=0
+        "/home/gil/projects/Alpha/modelcomp-appendix/data/abundance.csv", index_col=0
     )
-    imported_meta = pd.read_csv(os.getcwd() + "/../../data/meta.csv", index_col=0)
-    imported_abundance = mc.remove_falsy_columns(imported_abundance)
-    imported_abundance = mc.remove_rare_features(imported_abundance)
+    imported_meta = pd.read_csv(
+        "/home/gil/projects/Alpha/modelcomp-appendix/data/meta.csv", index_col=0
+    )
+    imported_abundance = mc.utils.remove_falsy_columns(imported_abundance)
+    imported_abundance = mc.utils.remove_rare_features(imported_abundance)
     # reading data from csv
     positive_uniques = imported_meta["PatientGroup"].unique().tolist()
     positive_uniques.sort()
     positive_uniques.remove("0")
     # patient group possible results list creation
-    X, y = mc.remove_string_columns(imported_abundance).to_numpy(), mc.encode(
+    X, y = mc.utils.remove_string_columns(
+        imported_abundance
+    ).to_numpy(), mc.utils.encode(
         imported_meta.drop("SampleID", axis=1).to_numpy().ravel()
     )
-
-    mc.cross_val_models(
-        list(get_models(seed=42)),
-        get_k_fold(seed=42),
-        X,
-        y,
-        "multiclass",
-        mc.remove_string_columns(imported_abundance).columns,
-        explain=False,
-    )
-    print(f"Finished validation {validation_count} of 65")
-    validation_count = validation_count + 1
     # multiclass
-    abundance, meta = mc.filter_data(
+    abundance, meta = mc.utils.filter_data(
         imported_abundance, imported_meta, "0", labels=positive_uniques
     )
     # setup list of labels in data
-    abundance = mc.remove_string_columns(abundance)
+    abundance = mc.utils.remove_string_columns(abundance)
     X, y = abundance.to_numpy(), meta.drop("SampleID", axis=1).to_numpy().ravel()
-    mc.cross_val_models(
+    X, y = StandardScaler().fit_transform(X), mc.utils.encode(y)
+    mc_all = mc.storage.ModelComparison(
         list(get_models(seed=42)),
         get_k_fold(seed=42),
         X,
         y,
-        positive_uniques,
-        mc.remove_string_columns(imported_abundance).columns,
-        explain=False,
+        mc.utils.remove_string_columns(imported_abundance).columns,
     )
-    # validation - train & test on all labels
-    print(f"Finished validation {validation_count} of 65")
-    validation_count += 1
-    for train_index, train_label in enumerate(positive_uniques[1:]):
-        abundance, meta = mc.filter_data(
-            imported_abundance, imported_meta, "0", labels=[train_label]
-        )
-        # filtering labels by selected ones
-        abundance = mc.remove_string_columns(abundance)
-        X, y = abundance.to_numpy(), meta.drop("SampleID", axis=1).to_numpy().ravel()
-        mc.cross_val_models(
-            list(get_models(seed=42)),
-            get_k_fold(seed=42),
-            X,
-            y,
-            train_label,
-            mc.remove_string_columns(imported_abundance).columns,
-            explain=False,
-        )
-        # validation - train & test on the same label
-        print(f"Finished validation {validation_count} of 65")
-        validation_count = validation_count + 1
-        for test_label in (
-            positive_uniques[:train_index] + positive_uniques[train_index + 1 :]
-        ):
-            # iterating over positives without test label
-            abundance, meta = mc.filter_data(
-                imported_abundance,
-                imported_meta,
-                "0",
-                label1=train_label,
-                label2=test_label,
-            )
-            abundance, meta = abundance.drop("SampleID", axis=1), meta.drop(
-                "SampleID", axis=1
-            )
-            control_train, control_test = mc.split_array(
-                mc.get_label_indexes(meta, 0), seed=42
-            )
-            train, test = mc.get_label_indexes(meta, 1) + list(
-                meta.loc[control_train].index.values
-            ), mc.get_label_indexes(meta, 2) + list(meta.loc[control_test].index.values)
-            train.sort()
-            test.sort()
-            X_train, X_test, y_train, y_test = (
-                abundance.loc[train],
-                abundance.loc[test],
-                meta.loc[train],
-                meta.loc[test],
-            )
-            y_test["PatientGroup"] = y_test["PatientGroup"].map(
-                lambda x: 1 if x == 2 else x
-            )
-            mc.std_validation_models(
-                list(get_models(seed=42)),
-                X_train,
-                X_test,
-                y_train,
-                y_test,
-                train_label,
-                test_label,
-                mc.remove_string_columns(imported_abundance).columns,
-            )
-            print(f"Finished validation {validation_count} of 65")
-            validation_count = validation_count + 1
-    mc.general_plots(positive_uniques)
-    print("Finished plotting general plots")
-    print("Program finished")
+    mc_all.validate(
+        "accuracy_score",
+        "roc_curve",
+        "precision_recall_curve",
+        "f1_score",
+        "builtin_importance",
+        "shap_explainer",
+    )
+    mc_all.export("export", "mean", "std", plots=True)
+    mco = mc.storage.ModelComparison.from_path("export")
+    mco.export("itmaybeworksidk", "mean", "std", plots=True)
